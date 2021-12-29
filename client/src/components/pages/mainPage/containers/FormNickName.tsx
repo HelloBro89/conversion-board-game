c9e3452e446c34290b4814120f01451f4212de18
Pavel <pavelvaloshyn89@gmail.com>
2021-12-29 19:21:24 +0100

    feat: added roules for space to form nick-name

--- client/src/components/pages/mainPage/containers/FormNickName.tsx
+++ client/src/components/pages/mainPage/containers/FormNickName.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, MouseEvent /* useRef */ } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useStyles } from '../styleMainPage';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,6 +7,7 @@ import { setNickName } from '../../../redux/actions/nickNameAction';
 import { RootState } from '../../../redux';
 
 const FormNickName = () => {
+    // const test = useRef<HTMLAnchorElement>(null);
     const classes = useStyles();
     const searchNickName = useSelector((state: RootState) => state.nickName);
     const dispatch = useDispatch();
@@ -14,14 +15,24 @@ const FormNickName = () => {
     const handlerTextField = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         e.preventDefault();
         const nickValue = e.target.value;
-        if (nickValue.trim() === ' ') {
+        const regexp = /^\S/;
+        const matchedValue = nickValue.match(regexp);
+        // console.log(matchedValue);
+
+        if (matchedValue) {
+            dispatch(setNickName(nickValue));
+        } else {
+            e.target.setSelectionRange(0, 0);
+            dispatch(setNickName(''));
+        }
+    };
+
+    const getNickName = (e: MouseEvent) => {
+        if (searchNickName.nickName.trim() === '') {
+            e.preventDefault();
             alert('Enter your nick-name!');
             return;
         }
-        dispatch(setNickName(nickValue));
-    };
-
-    const getNickName = () => {
         console.log(searchNickName.nickName);
     };
 
@@ -33,9 +44,11 @@ const FormNickName = () => {
                 id="filled-basic"
                 label="Enter your nick-name"
                 variant="filled"
+                autoComplete="off"
+                value={searchNickName.nickName}
             />
             <nav>
-                <NavLink to="/lobby" type="button" onClick={getNickName} className={classes.navStyle}>
+                <NavLink to="/lobby" /* ref={test} */ /*  id="send" */ onClick={(e) => getNickName(e)} className={classes.navStyle}>
                     Enter
                 </NavLink>
             </nav>
